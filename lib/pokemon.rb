7adb93bf742241d0f524d584501899cb7e9171f1
David Cantoran <davidcantoran36@gmail.com>
2020-01-17 01:23:44 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,21 +11,14 @@ class Pokemon
     db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end 
   
-  def self.find(num, db)
-    # poke = db.execute("SELECT * FROM pokemon WHERE id = ?", [lookup_id])
-    # new_pokemon = self.new(poke[0][0], poke[0][1], poke[0][2], poke[0][3])
-    # # new_pokemon[:id] = poke[0][0]
-    # # new_pokemon[:name] = poke[0][1]
-    # # new_pokemon[:type] = poke[0][2]
-    # # new_pokemon[:db] = poke[0][3]
-    # new_pokemon
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", [num])
-    new_pokemon = self.new(pokemon)
-    new_pokemon.id = pokemon[0][0]
-    new_pokemon.name = pokemon[0][1]
-    new_pokemon.type = pokemon[0][2]
-    new_pokemon.hp = pokemon[0][3]
-    return new_pokemon
+  def self.find(lookup_id, db)
+    poke = db.execute("SELECT * FROM pokemon WHERE id = ?", [lookup_id])
+    new_pokemon = self.new(poke[0][0], poke[0][1], poke[0][2], poke[0][3])
+    # new_pokemon[:id] = poke[0][0]
+    # new_pokemon[:name] = poke[0][1]
+    # new_pokemon[:type] = poke[0][2]
+    # new_pokemon[:db] = poke[0][3]
+    new_pokemon
   end 
   
 end
